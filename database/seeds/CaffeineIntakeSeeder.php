5ad32bee494e5cc176ae56c9e9fa8df68c2234c6
Shawn Azar <cydiasa@gmail.com>
2018-10-19 00:15:44 -0700

    added some test data seeder for Caffeine Intakes

---
+++ database/seeds/CaffeineIntakeSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+
+class CaffeineIntakeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i=0; $i < 10; $i++) {
+            DB::table('caffeine_intakes')->insert([
+                'user_id'               => 1,
+                'caffeine_source_id'    => rand(1,17),
+                'units'                 => rand(1,5),
+                'created_at'            => Carbon::now()->today()
+            ]);
+
+            DB::table('caffeine_intakes')->insert([
+                'user_id'               => 1,
+                'caffeine_source_id'    => rand(1,17),
+                'units'                 => rand(1,5),
+                'created_at'            => Carbon::now()->subWeek()
+            ]);
+
+            DB::table('caffeine_intakes')->insert([
+                'user_id'               => 1,
+                'caffeine_source_id'    => rand(1,17),
+                'units'                 => rand(1,5),
+                'created_at'            => Carbon::now()->subYear()
+            ]);
+
+            DB::table('caffeine_intakes')->insert([
+                'user_id'               => 1,
+                'caffeine_source_id'    => rand(1,17),
+                'units'                 => rand(1,5),
+                'created_at'            => Carbon::now()->subYears(2)
+            ]);
+
+        }
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(CaffeineSourceSeeder::class);
+        $this->call(CaffeineIntakeSeeder::class);
     }
 }
